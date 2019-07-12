9215b3aee943ada46fa3ec19cf8d2761d2221eee
shanyao <sqshanyao@163.com>
2019-07-12 09:51:00 +0800

    mapper.xml自定义javaType

---
+++ src/main/java/com/zzg/mybatis/generator/tst/TstMain.java
@@ -0,0 +1,15 @@
+package com.zzg.mybatis.generator.tst;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+
+/**
+ * @create by  单耀
+ * @create date  2019/7/11
+ */
+public class TstMain {
+    public static void main(String[] args) {
+        FullyQualifiedJavaType fullyQualifiedJavaType = new FullyQualifiedJavaType("com.yao.TestType");
+        System.out.println(fullyQualifiedJavaType);
+
+    }
+}
